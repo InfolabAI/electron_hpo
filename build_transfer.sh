0c65e35c98b15053632437011b0f80c75978adbf
hee <skyde1021@dgist.ac.kr>
2025-03-21 15:42:54 +0900

    fix

--- build_transfer.sh
+++
@@ -1,8 +0,0 @@
-path="/data3/share/2503_HPOptimizer"
-file1="./dist/HPOptimizer Setup 1.0.0.exe"
-file2="./python_scripts/test_client.py"
-file3="./pure_cpp_scripts"
-file4="../HPOptimizer 메뉴얼.pdf"
-file5="../cpp_codes/Undistortion"
-
-scp -r "$file1" "$file2" "$file3" "$file4" "$file5" "robert.lim@156.147.154.18:$path"
